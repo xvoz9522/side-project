d530b64888ef2e3aa7d05370715804285fc615da
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-09-17 13:16:01 +0800

    Add files via upload

---
+++ test_8_15/test_8_15/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("GO\n");
+//	return 0;
+//   
+//}
+//int main()
+//{
+//	char ch = 'a'; 
+//	printf("%c\n", ch);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum);
+
+	return 0;
+}
