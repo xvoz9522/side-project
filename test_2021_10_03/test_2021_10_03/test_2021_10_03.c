7c098f88bcd4a787fb6e9fad304a3f12891f2391
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-10-03 17:37:30 +0800

    side-project

---
+++ test_2021_10_03/test_2021_10_03/test_2021_10_03.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+//int GOD = 2021;
+
+//int main()
+//{
+//	printf("1:%d\n", GOD);
+//	printf("good\n"); 
+//	{
+//		printf("2:%d\n", GOD);
+//		int a = 10;
+//		printf("a = %d\n", a);
+//	}
+//	printf("3:%d\n", GOD);
+//	return 0;
+//
+//}
+
+extern int GOD;
+
+int main() {
+	printf("%d\n", GOD);
+
+	return 0;
+}
